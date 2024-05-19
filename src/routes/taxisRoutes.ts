7f51f90bc38c83d4aeae93296d02dce35a83f893
N4T4LY <55033104+N4T4LY@users.noreply.github.com>
2024-05-19 16:05:19 -0400

    feat(taxisRoutes.ts): taxisRouter module with route handlers

---
+++ src/routes/taxisRoutes.ts
@@ -0,0 +1,12 @@
+import express, { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
+import {
+  getTaxis,
+  getTaxi,
+  createTaxi,
+  updateTaxi,
+  deleteTaxi,
+} from "../controller/taxisController";
+
+import { getTrayectoriesByTaxi } from "../controller/trajectoriesController";
+export const taxisRouter = express.Router();
