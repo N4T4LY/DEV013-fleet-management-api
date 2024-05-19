9c54a3259d038e50a6bd8ce4a6b20bf0ca8e86a2
N4T4LY <55033104+N4T4LY@users.noreply.github.com>
2024-05-19 19:59:36 -0400

    test(taxis.spec.ts): mock dbServer in taxisRouter.test.ts

--- test/unit/taxis.spec.ts
+++ test/unit/taxis.spec.ts
@@ -4,3 +4,16 @@ import { getTaxis,getTaxi, createTaxi, updateTaxi, deleteTaxi} from '../../src/c
 import { Request, Response } from 'express';
 //importamos prisma de dbserver para simular las interacciones con la BD
 import prisma from '../../src/utils/dbServer';
+
+//aqui moqueamos dbserver para reemplazar prisma un objetos simulados , jest.fn crea funciones simuladas, 
+jest.mock('../../src/utils/dbServer', () => ({
+  
+  taxis: {
+    //creamos funciones simuladas de prisma usando jestfn
+    findMany: jest.fn(),
+    findUnique: jest.fn(),
+    create: jest.fn(),
+    update: jest.fn(),
+    delete: jest.fn(),
+  },
+}));
