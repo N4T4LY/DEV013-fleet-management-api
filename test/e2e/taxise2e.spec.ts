17c581aebe3207fbff488eaa2aba9139c41e8b50
N4T4LY <55033104+N4T4LY@users.noreply.github.com>
2024-05-19 19:37:28 -0400

    test(taxise2e.spec.ts): add tests for GET /taxis endpoint

--- test/e2e/taxise2e.spec.ts
+++ test/e2e/taxise2e.spec.ts
@@ -1,2 +1,26 @@
 import request from "supertest"
 import app from "../../src/app"
+describe('GET /taxis', () => {
+  it('should get taxis', async () => {
+    const response = await request(app).get('/taxis?limit=11&page=0');
+    expect(response.status).toBe(200);
+  });
+
+  it('should return the correct number of taxis on the pagination', async () => {
+    const response = await request(app).get('/taxis?limit=1&page=0');
+    expect(response.body.length).toBe(1);
+    console.log(response.body.length)
+  });
+
+  it('should return 400 if limit is not a positive integer', async () => {
+    const response = await request(app).get('/taxis?limit=-5&page=0');
+    expect(response.status).toBe(400);
+  });
+
+  it('should return 400 if page is negative', async () => {
+    const response = await request(app).get('/taxis?limit=10&page=-5');
+    expect(response.status).toBe(400);
+  });
+
+  
+});
