40b7e3d13bbd1a7ab6d617b6ae79698d85ecf8d7
N4T4LY <55033104+N4T4LY@users.noreply.github.com>
2024-05-19 18:39:22 -0400

    docs(trajectoriesRoutes.ts): add Swagger schema for Trajectories model

--- src/routes/trajectoriesRoutes.ts
+++ src/routes/trajectoriesRoutes.ts
@@ -3,4 +3,50 @@ import { body, validationResult } from "express-validator"
 import { getTrayectoriesByTaxi, getLastReportedLocations } from "../controller/trajectoriesController"
 
 export const trajectoriesRouter = express.Router()
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Trajectories:
+ *       type: object
+ *       properties:
+ *         id:
+ *           type: integer
+ *           description: ID de la trayectoria
+ *         taxi_id:
+ *           type: integer
+ *           description: ID del taxi
+ *         date:
+ *           type: string
+ *           description: Fecha y hora de la ubicación reportada
+ *         latitude:
+ *           type: number
+ *           format: float
+ *           description: Latitud de la ubicación
+ *         longitude:
+ *           type: number
+ *           format: float
+ *           description: Longitud de la ubicación
+ *       required:
+ *         - id
+ *         - taxi_id
+ *       example:
+ *         id: 27704
+ *         taxi_id: 7957
+ *         date: "2008-02-08T17:10:16.000Z"
+ *         latitude: 116.23249
+ *         longitude: 39.91784
+ *         taxis:
+ *           id: 7957
+ *           plate: "BAJW-7971"
+ */
+
+
+                
+/**
+ * @swagger
+ * tags:
+ *     - name: Trajectories Query 
+ *       description: Query endpoints
+ */
 trajectoriesRouter.get("/",getLastReportedLocations);
