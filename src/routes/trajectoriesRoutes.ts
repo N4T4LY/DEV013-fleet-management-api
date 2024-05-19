98c6ad23928a8e8c632a51a5bcfba7b91ddb2b05
N4T4LY <55033104+N4T4LY@users.noreply.github.com>
2024-05-19 18:32:08 -0400

    feat(trajectoriesRoutes.ts): add trajectories router

--- src/routes/trajectoriesRoutes.ts
+++ src/routes/trajectoriesRoutes.ts
@@ -1,3 +1,5 @@
 import express, { Request, Response } from "express"
 import { body, validationResult } from "express-validator"
 import { getTrayectoriesByTaxi, getLastReportedLocations } from "../controller/trajectoriesController"
+
+export const trajectoriesRouter = express.Router()
