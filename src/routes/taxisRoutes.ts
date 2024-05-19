88340a71ae57d23143cf28e579c2ef41a61d7d93
N4T4LY <55033104+N4T4LY@users.noreply.github.com>
2024-05-19 16:15:36 -0400

    feat(taxisRoutes.ts): add route to create a new taxi with plate validation

--- src/routes/taxisRoutes.ts
+++ src/routes/taxisRoutes.ts
@@ -13,3 +13,14 @@ export const taxisRouter = express.Router();
 
 taxisRouter.get("/", getTaxis);
 taxisRouter.get("/:id", getTaxi);
+taxisRouter.post(
+  "/",
+  body("plate").isString(),
+  async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    return await createTaxi(req, res);
+  }
+);
