b227c67e25206db001301c522063586bccf05598
N4T4LY <55033104+N4T4LY@users.noreply.github.com>
2024-05-19 21:31:54 -0400

    test(trajectories.spec.ts): add unit test for negative limit or page in getTrajectoriesByTaxi

--- test/unit/trajectories.spec.ts
+++ test/unit/trajectories.spec.ts
@@ -13,4 +13,13 @@ describe('getTrajectoriesByTaxi', () => {
     expect(mockRes.status).toHaveBeenCalledWith(400);
     expect(mockRes.json).toHaveBeenCalledWith({ error: "taxiId, date, limit and page are required" });
   });
+  it('should return 400 if limit or page are not positive numbers', async () => {
+    const mockReq = { params: { taxiId: '1' }, query: { date: '2024-05-15', limit: '-1', page: '0' } } as unknown as Request;
+    const mockRes = { status: jest.fn().mockReturnThis(), json: jest.fn() } as unknown as Response;
+
+    await getTrajectoriesByTaxi(mockReq, mockRes);
+
+    expect(mockRes.status).toHaveBeenCalledWith(400);
+    expect(mockRes.json).toHaveBeenCalledWith({ error: "limit and page must be positive" });
+  });
 })
