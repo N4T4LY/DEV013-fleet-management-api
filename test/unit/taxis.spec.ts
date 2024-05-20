2b2aa8802636e3e58fb74a821130b9f063c477cc
N4T4LY <55033104+N4T4LY@users.noreply.github.com>
2024-05-19 20:01:40 -0400

    test(taxis.spec.ts): add test for pagination in getTaxis function

--- test/unit/taxis.spec.ts
+++ test/unit/taxis.spec.ts
@@ -17,3 +17,25 @@ jest.mock('../../src/utils/dbServer', () => ({
     delete: jest.fn(),
   },
 }));
+
+describe('getTaxis', () => {
+  it('should return taxis with pagination', async () => {
+    //simulamos la respuesta que esperamos que suceda
+    const mockTaxis = [{ id: 1, plate: 'ABC123' }, { id: 2, plate: 'DEF456' }];
+    //simulamos la respuesta y el request con valores simulados. 
+    const mockReq = { query: { limit: '2', page: '0' } } as unknown as Request;
+    const mockRes = { status: jest.fn().mockReturnThis(), json: jest.fn() } as unknown as Response;
+
+    (prisma.taxis.findMany as jest.Mock).mockResolvedValue(mockTaxis);
+
+    await getTaxis(mockReq, mockRes);
+
+    expect(mockRes.status).toHaveBeenCalledWith(200);
+    expect(mockRes.json).toHaveBeenCalledWith(mockTaxis);
+    expect(prisma.taxis.findMany).toHaveBeenCalledWith({
+      select: { id: true, plate: true },
+      skip: 0,
+      take: 2,
+    });
+  });
+})
